45a84c515ca09d9e2cdd086a370069dcc7282364
Matt Gauger <matt.gauger@gmail.com>
2013-12-22 21:11:36 -0600

    Cleanup

--- project.clj
+++ project.clj
@@ -3,19 +3,24 @@
   :url "https://github.com/mathias/aegia"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [io.pedestal/pedestal.service "0.1.1-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2120"]]
 
-                 ;; Remove this line and uncomment the next line to
-                 ;; use Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.1.1-SNAPSHOT"]
-                 ;; [io.pedestal/pedestal.tomcat "0.0.1-SNAPSHOT"]
+  :plugins [[lein-cljsbuild "0.3.3"]]
+  ;; cljsbuild tasks configuration
+  :cljsbuild {:builds
+              [{;; clojurescript source code path
+                :source-paths ["src/"]
 
-                 ;; Logging
-                 [ch.qos.logback/logback-classic "1.0.7"]
-                 [org.slf4j/jul-to-slf4j "1.7.2"]
-                 [org.slf4j/jcl-over-slf4j "1.7.2"]
-                 [org.slf4j/log4j-over-slf4j "1.7.2"]]
-  :profiles {:dev {:source-paths ["dev"]}}
-  :resource-paths ["config"]
-  :main ^{:skip-aot true} aegia.server)
+                ;; Google Closure Compiler options
+                :compiler {;; the name of the emitted JS file
+                           :output-to "application.js"
+
+                           ;; use minimal optimization CLS directive
+                           :optimizations :simple
+
+                           ;; prettyfying emitted JS
+                           :pretty-print true
+
+                           ;; compile for node
+                           :target :nodejs}}]})
